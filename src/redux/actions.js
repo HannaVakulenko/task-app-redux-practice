c392d56d437f58c37c2e6d3e501fcca677760de4
Hanna Vakulenko <anna.vakulenko.ua@gmail.com>
2023-05-24 13:48:29 +0300

    refactoring code using redux-toolkit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,33 +1,52 @@
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
+import { createAction, nanoid } from '@reduxjs/toolkit';
 
-export const addTask = text => {
+//=============== Before ======================== using redux
+
+// export const addTask = text => {
+//   return {
+//     type: 'tasks/addTask',
+//     payload: {
+//       id: nanoid(),
+//       completed: false,
+//       text,
+//     },
+//   };
+// };
+
+// export const deleteTask = taskId => {
+//   return {
+//     type: 'tasks/deleteTask',
+//     payload: taskId,
+//   };
+// };
+
+// export const toggleCompleted = taskId => {
+//   return {
+//     type: 'tasks/toggleCompleted',
+//     payload: taskId,
+//   };
+// };
+
+// export const setStatusFilter = value => {
+//   return {
+//     type: 'filters/setStatusFilter',
+//     payload: value,
+//   };
+// };
+
+//=============== After ======================== using redux-toolkit
+
+export const addTask = createAction('tasks/addTask', text => {
   return {
-    type: 'tasks/addTask',
     payload: {
+      text,
       id: nanoid(),
       completed: false,
-      text,
     },
   };
-};
+});
 
-export const deleteTask = taskId => {
-  return {
-    type: 'tasks/deleteTask',
-    payload: taskId,
-  };
-};
-
-export const toggleCompleted = taskId => {
-  return {
-    type: 'tasks/toggleCompleted',
-    payload: taskId,
-  };
-};
-
-export const setStatusFilter = value => {
-  return {
-    type: 'filters/setStatusFilter',
-    payload: value,
-  };
-};
+export const deleteTask = createAction('tasks/deleteTask');
+export const toggleCompleted = createAction('tasks/toggleCompleted');
+export const setStatusFilter = createAction('filters/setStatusFilter');
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,22 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
+//before - using redux
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { rootReducer } from './reducer';
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+
+//after - using redux-toolkit
+import { configureStore } from '@reduxjs/toolkit';
+// import { tasksReducer, filtersReducer } from './reducer';
+import { tasksReducer } from './taskSlice';
+import { filtersReducer } from './filterSlice';
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
+
+// action.js та reducer.js вже непотрібні
